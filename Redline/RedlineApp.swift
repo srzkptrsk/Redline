c6a1adacb9de413edff3e17fb17b2903b81aa34e
Siaržuk Piatroŭski <siarzuk@piatrouski.com>
2026-01-12 06:24:52 +0100

    Initial Commit

---
+++ Redline/RedlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedlineApp.swift
+//  Redline
+//
+//  Created by Siarhei Piatrouski on 12/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct RedlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
